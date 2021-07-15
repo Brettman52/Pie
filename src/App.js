4325e4e3e80424fec6b53ece5d37477618305534
Brett Adams <Bretman52@yahoo.com>
2021-07-15 15:14:38 -0500

    added useState hook to App.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,32 @@
 import './App.css';
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import PiePicker from './PiePicker';
 import PieDesc from './PieDesc';
 import pieData from './pieData'
 import PieContext from './PieContext'
 
-export default class App extends Component {
-
-    state = {
-        pie: "",
-        desc: ""
-    }
-
-    setDesc = (pieKey, index) => {
+export default function App() {
+ 
+    const [pieInfo, setPieInfo] = useState({pie: "", desc: ""})
+    const setDesc = (pieKey, index) => {
         const pieId = pieData[index][pieKey];
-        this.setState({pie: pieId.displayName, desc: pieId.desc})
+        setPieInfo({pie: pieId.displayName, desc: pieId.desc});
     }
 
-    render() {
-        const contextValue = {
-            pieData: pieData,
-            pie: this.state.pie,
-            desc: this.state.desc,
-            handleClick: this.setDesc
-        }
+    const contextValue = {
+        pieData: pieData,
+        pie: pieInfo.pie,
+        desc: pieInfo.desc,
+        handleClick: setDesc
+    }
 
-        return (
-            <div>
-                <PieContext.Provider value={contextValue}>
+    return (
+        <div>
+            <PieContext.Provider value={contextValue}>
                     <PiePicker pieData={pieData}/>
                     <PieDesc/>
                 </PieContext.Provider>
-            </div>
-        )
-    }
+        </div>
+    )
 }
-
-
-// export default function App() {
-//     state = {
-//         pie: "",
-//         desc: ""
-//     }
-
-
-//     const [pieInfo, setPieInfo] = useState()
-//     const setDesc = (pieKey, index) => {
-//         const pieId = pieData[index][pieKey];
-//         const {pie, desc} = pieInfo;
-//         setPieInfo({pie: pieId.displayName, desc: pieId.desc});
-//     }
-
-
-//     const contextValue = {
-//         pieData: pieData,
-//         pie: state.pie,
-//         desc: state.desc,
-//         handleClick: setDesc
-//     }
-
-//     return (
-//         <div>
-//             <PieContext.Provider value={contextValue}>
-//                     <PiePicker pieData={pieData}/>
-//                     <PieDesc/>
-//                 </PieContext.Provider>
-//         </div>
-//     )
-// }
 
